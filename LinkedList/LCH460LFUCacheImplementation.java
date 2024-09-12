4637c44a8f831732b1a77b62cb67ffee4d292311
Charanjit <charanjitnandigama24@gmail.com>
2024-09-13 00:31:06 +0530

    aded in LL's folder

---
+++ LinkedList/LCH460LFUCacheImplementation.java
@@ -0,0 +1,125 @@
+
+// Used  2 maps and in one map  we store DoubleLinkedLists.
+
+class LCH460LFUCacheImplementation {
+    int capacity;
+    int curSize;
+    int minFrequency;
+    Map<Integer, DLLNode> cache;
+    Map<Integer, DoubleLinkedList> frequencyMap;
+
+
+    public LCH460LFUCacheImplementation(int capacity) {
+        this.capacity = capacity;   
+        this.curSize = 0;
+        this.minFrequency = 0;
+
+        this.cache = new HashMap<>();
+        this.frequencyMap = new HashMap<>();
+
+    }
+    public void updateNode(DLLNode curNode){
+        int curFreq = curNode.useCounter;
+        DoubleLinkedList curDLL = frequencyMap.get(curFreq);
+        curDLL.removeNode(curNode);
+
+        if(curFreq == minFrequency && curDLL.listSize == 0){
+            minFrequency++;
+        }
+
+        curNode.useCounter++;
+
+        DoubleLinkedList newDLL = frequencyMap.getOrDefault(curNode.useCounter, new DoubleLinkedList());
+        newDLL.addNode(curNode);
+        frequencyMap.put(curNode.useCounter, newDLL);
+    }
+    
+    public int get(int key) {
+        if(!cache.containsKey(key))  return -1;
+        DLLNode curNode  = cache.get(key);
+        updateNode(curNode);
+        return curNode.val;
+    }
+    
+    public void put(int key, int value) {
+        if(capacity ==0) return;
+        if(cache.containsKey(key)){
+            DLLNode curNode = cache.get(key);
+            curNode.val = value;
+            updateNode(curNode);
+        }
+        else{
+            curSize++;
+            if(curSize> capacity){
+                DoubleLinkedList minFreqDLL = frequencyMap.get(minFrequency);
+                
+                cache.remove(minFreqDLL.tail.prev.key);
+                minFreqDLL.removeNode(minFreqDLL.tail.prev);
+
+                curSize--;
+            }
+            minFrequency = 1;
+            DLLNode newNode = new DLLNode(key, value);
+
+            DoubleLinkedList curDLL = frequencyMap.getOrDefault(1, new DoubleLinkedList());
+            curDLL.addNode(newNode);
+            frequencyMap.put(1,curDLL);
+            cache.put(key, newNode);
+
+        }
+        
+    }
+}
+
+class DLLNode{
+    int key;
+    int val;
+    int useCounter;
+    DLLNode prev;
+    DLLNode next;
+    public DLLNode(int key, int val){
+        this.key = key;
+        this.val = val;
+        this.useCounter = 1;
+    }
+}
+
+class DoubleLinkedList{
+    int listSize;
+    DLLNode head;
+    DLLNode tail;
+    public DoubleLinkedList(){
+        this.listSize =0;
+        this.head = new DLLNode(0,0);
+        this.tail = new DLLNode(0,0);
+        head.next = tail;
+        tail.prev = head;
+    }
+
+    public void addNode(DLLNode curNode){
+        DLLNode initialAfterHead = head.next;
+        curNode.next = initialAfterHead;
+        curNode.prev = head;
+        head.next = curNode;
+        initialAfterHead.prev = curNode;
+        listSize++;
+    }
+
+    public void removeNode(DLLNode curNode){
+        DLLNode prevNode = curNode.prev;
+        DLLNode afterNode = curNode.next;
+
+        prevNode.next = afterNode;
+        afterNode.prev = prevNode;
+        listSize--;
+    }
+
+
+}
+
+/**
+ * Your LFUCache object will be instantiated and called as such:
+ * LFUCache obj = new LFUCache(capacity);
+ * int param_1 = obj.get(key);
+ * obj.put(key,value);
+ */
---
+++ LinkedList/LCM146LRUCacheImplementation.java
@@ -0,0 +1,82 @@
+class Node{
+    int key;
+    int val;
+    Node next;
+    Node prev;
+    Node(int key,int val){
+        this.key = key;
+        this.val = val;
+    }
+}
+class LCM146LRUCacheImplementation {
+    // Used a map and DoubleLinkedList
+
+    // SC->O(capacity)
+
+    Map<Integer,Node> map;
+    int cap;
+    Node head;
+    Node tail;
+
+    
+    public LCM146LRUCacheImplementation(int capacity) {     // TC->O(1)
+        map = new HashMap<>();
+        cap = capacity;
+       
+        head = new Node(-1,-1);
+        tail = new Node(-1,-1);
+        head.next = tail;
+        tail.prev = head;
+        
+    }
+    public void insertAfterHead(Node nd){   // TC->O(1)
+        Node currentAfterHead = head.next;
+        head.next = nd;
+        nd.next = currentAfterHead;
+        nd.prev = head;
+        currentAfterHead.prev = nd;
+    }
+
+    public void deleteNode(Node nd){    // TC->O(1)
+        Node prevNode = nd.prev;
+        Node afterNode = nd.next;
+        prevNode.next = afterNode;
+        afterNode.prev = prevNode;
+    }
+    
+    public int get(int key) {      // TC->O(1) Assuming map works in O(1)
+        if(!map.containsKey(key)) return -1;
+        Node nd = map.get(key);
+        deleteNode(nd);
+        insertAfterHead(nd);
+        return nd.val;
+    }
+    
+    public void put(int key, int value) {    // TC->O(1) Assuming map works in O(1)
+        if(map.containsKey(key)){
+            Node nd = map.get(key);
+            nd.val = value;
+            deleteNode(nd);
+            insertAfterHead(nd);
+        }
+        else{
+            if(map.size()==cap){
+                Node LRU = tail.prev;
+                map.remove(LRU.key);    
+                deleteNode(LRU);
+            }
+            Node newNode = new Node(key, value);
+            map.put(key,newNode);
+            insertAfterHead(newNode);
+
+        }
+        
+    }
+}
+
+/**
+ * Your LRUCache object will be instantiated and called as such:
+ * LRUCache obj = new LRUCache(capacity);
+ * int param_1 = obj.get(key);
+ * obj.put(key,value);
+ */

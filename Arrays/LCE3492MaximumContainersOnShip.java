f8396768b7ae83e916f176a798aa173877bf3d55
Charanjit <charanjitnandigama24@gmail.com>
2025-10-20 10:59:49 +0530

    del

--- Arrays/LCE3492MaximumContainersOnShip.java
+++
@@ -1,6 +0,0 @@
-//TC->O(1), SC->O(1)
-class LCE3492MaximumContainersOnShip {
-    public int maxContainers(int n, int w, int maxWeight) {
-        return (n*n*w)<maxWeight ?  n*n : maxWeight/w;
-    }
-}
--- Strings/LCE58LengthofLastWord.java
+++
@@ -1,8 +0,0 @@
-class LCE58LengthofLastWord {
-    // TC->O(n)
-    public int lengthOfLastWord(String s) {
-        String[] arr = s.split("\\s+");
-        return arr[arr.length-1].length();
-        
-    }
-}
